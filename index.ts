434025873cc4da0f7406676fd0b671940dbc8cbc
yzzting <yzz1995@aliyun.com>
2024-01-26 12:34:39 +0800

    feat: hasPackageJson and inquirer select script

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@
  * @Author: ZenYang
  * @Date: 2024/1/25 17:02
  */
+import path from 'node:path'
+import fs from 'node:fs'
 import { Command } from 'commander'
+import { select } from '@inquirer/prompts'
 
 const program = new Command()
 
@@ -25,12 +28,51 @@ function getPackageInfo(): { version: string, description: string, scripts: IScr
 
 const { version, description, scripts } = getPackageInfo()
 
+/**
+ * @description: check current directory has package.json
+ * @return boolean
+ */
+function hasPackageJson(): boolean {
+  const packageJsonPath = path.join(process.cwd(), 'package.json')
+  try {
+    fs.accessSync(packageJsonPath, fs.constants.F_OK)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+// transform scripts to array for inquirer { value: string, name: string }
+const scriptChoices = Object.keys(scripts).map((key) => ({
+  value: key,
+  name: `${key}: ${scripts[key]}`
+}))
+
+// create a prompt for the user to choose a script
+const scriptPrompt = {
+  message: 'Which script would you like to run?',
+  choices: scriptChoices
+}
+
+/**
+ * @description: Use the prompt to get the script name
+ * @return void
+ */
+async function getScriptName(){
+  const selectScript = await select(scriptPrompt)
+  console.log(selectScript)
+}
+
 program
   .name('ScriptSleuth')
   .version(version)
   .description(description)
   .action(() => {
-    console.log(scripts)
+    if (!hasPackageJson()) {
+      console.log('No package.json found')
+      process.exit(1)
+    }
+    getScriptName()
   })
 
 program.parse(process.argv)
